0b90707d6eddd822815d7c26939869955d75cabb
ZStrikeXIII <jacksonturner1999@gmail.com>
2020-04-04 13:54:50 -0400

    Version 2.1: Fixed exceptions in Controller class, added comments and JavaDoc comments to code.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,12 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 
+/**
+ * Controller class to handle all of the events that will occur in the system, mainly three button
+ * clicks that will perform varying actions.
+ *
+ * @author jbturner
+ */
 public class Controller {
   //FX ID Setups
 
@@ -60,44 +66,64 @@ public class Controller {
   @FXML
   private Button removeButton;
 
+  /**
+   * Method to handle the actions that will occur when the Adjust Par Level button is clicked.
+   *
+   * @param actionEvent The button being clicked is the actionEvent that will make the method
+   *                    occur.
+   */
   public void handleParButtonAction(ActionEvent actionEvent) {
-    //try{
-    String newPar = newParField.getText();
-    parLevelField.setText(newPar);
-    System.out.println("Par Level Adjustment Confirmed");
+    //try{ //Try and catch blocks were not needed.
+    String newPar = newParField.getText(); //Getting new Par Level
+    parLevelField.setText(newPar); //Setting new Par Level
+    System.out.println("Par Level Adjustment Confirmed"); //Confirmation of process
     /*} catch (SQLException e) {
       e.printStackTrace();
     }*/
   }
 
+  /**
+   * Method to handle the actions that will occur when the Add Items button is clicked.
+   *
+   * @param actionEvent The button being clicked is the actionEvent that will make the method
+   *                    occur.
+   */
   public void handleAddButtonAction(ActionEvent actionEvent) {
     try {
-      String initValueString = itemField3.getText();
-      int initValue = Integer.parseInt(initValueString);
-      String addValueString = addRemoveItemField.getText();
+      String initValueString = itemField3.getText(); //Getting item's initial value in the system
+      int initValue = Integer.parseInt(initValueString); //Changing String to int
+      String addValueString = numItems.getText();//Getting value to be added to initial value
+      //System.out.println(addValueString);
       int addValue = Integer.parseInt(addValueString);
-      int newValue = initValue + addValue;
-      String newValueString = Integer.toString(newValue);
-      itemField3.setText(newValueString);
-      System.out.println("Added Items Confirmed");
+      int newValue = initValue + addValue; //Adding
+      String newValueString = Integer.toString(newValue); //Converting back to String
+      itemField3.setText(newValueString); //Setting new item value
+      System.out.println("Added Items Confirmed"); //Confirmation of process
     } catch (NumberFormatException e) {
       e.printStackTrace();
     }
   }
 
+  /**
+   * Method to handle the actions that occur when the Remove Items button is clicked.
+   *
+   * @param actionEvent The button being clicked is the actionEvent that will make the method
+   *                    occur.
+   */
   public void handleRemoveButtonAction(ActionEvent actionEvent) {
     try {
-      String initValueString = itemField3.getText();
-      int initValue = Integer.parseInt(initValueString);
-      String removeValueString = addRemoveItemField.getText();
+      String initValueString = itemField3.getText(); //Getting item's initial value in the system
+      int initValue = Integer.parseInt(initValueString); //Converting to int
+      String removeValueString = numItems
+          .getText(); //Getting value to be removed from initial value
       int removeValue = Integer.parseInt(removeValueString);
-      int newValue = initValue + removeValue;
-      if (newValue < 0) {
+      int newValue = initValue - removeValue; //Subtracting
+      if (newValue < 0) { //Checking if result is negative
         System.out.println("Cannot have a Negative Amount of Items");
-      } else {
-        String newValueString = Integer.toString(newValue);
-        itemField3.setText(newValueString);
-        System.out.println("Removal of Items Confirmed");
+      } else { //If result isn't negative...
+        String newValueString = Integer.toString(newValue); //Converting back to String
+        itemField3.setText(newValueString); //Setting new item value
+        System.out.println("Removal of Items Confirmed"); //Confirmation of process
       }
     } catch (NumberFormatException e) {
       e.printStackTrace();
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This program is a prototype for an inventory management system for my client, and is clickable
+ * with some functionality to demonstrate what the system will need to do.
+ *
+ * @author jbturner
+ */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 372, 248));
-        primaryStage.show();
-    }
+  /**
+   * The start point of the JavaFX program.
+   *
+   * @param primaryStage The primary stage.
+   * @throws Exception Any errors or exceptions that may occur during execution.
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Inventory Management System");
+    primaryStage.setScene(new Scene(root, 372, 248));
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
